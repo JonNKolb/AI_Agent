c0df1e0e51de54fc61019a243ffa61d36d8c088a
LeahKolb <Jonked@gmail.com>
2025-10-12 19:06:45 -0400

    1: Chapter 2 Functions

---
+++ functions/get_file_content.py
@@ -0,0 +1,19 @@
+import os
+from config import MAX_CHARS
+
+def get_file_content(working_directory, file_path):
+    work_path = os.path.join(working_directory, file_path)
+    abs_path = os.path.abspath(work_path)
+    abs_work = os.path.abspath(working_directory)
+    if not abs_path.startswith(abs_work):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+    if not os.path.isfile(work_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+    try:
+        with open(work_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if os.path.getsize(abs_path) > MAX_CHARS:
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+            return file_content_string
+    except Exception as e:
+        return f"Error getting file contents: {e}"
---
+++ functions/get_files_info.py
@@ -0,0 +1,21 @@
+import os
+
+def get_files_info(working_directory, directory="."):
+    work_path = os.path.join(working_directory, directory)
+    abs_path = os.path.abspath(work_path)
+    abs_work = os.path.abspath(working_directory)
+    if not abs_path.startswith(abs_work):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    if not os.path.isdir(work_path):
+        return f'Error: "{directory}" is not a directory'
+    try:
+        contents = os.listdir(work_path)
+        output = []
+        for content in contents:
+            content_path = os.path.join(work_path, content)
+            bytes = os.path.getsize(content_path)
+            isdir = os.path.isdir(content_path)
+            output.append(f' - {content}: file_size={bytes}, is_dir={isdir}')
+        return "\n".join(output)
+    except Exception as e:
+        return f"Error listing files: {e}"
